5f9671d4f35d1d16fddfd59f8a1d7686f3a4ba91
Mark <markyanovskiy@gmail.com>
2023-11-02 11:00:29 +0100

    Used assignment operators

--- calculations.js
+++ calculations.js
@@ -24,3 +24,32 @@ console.log(decreaseBalance(3000, 700));
 console.log(divideBalanceByAccounts(3000, 2, 4000));
 console.log(getRestAfterDivision(7000, 3));
 // Second function "y" was missing to run a code
+
+function increaseBalance(x, y) {
+  x += y;
+  return x;
+}
+//console.log(increaseBalance(3000, 700));
+
+function decreaseBalance(x,y) {
+  x -=y;
+  return x;
+  }
+ // console.log(decreaseBalance(10, 5));
+
+
+function divideBalanceByAccounts(x, y) {
+  x /= y;
+  return x;
+}
+//console.log(divideBalanceByAccounts(10, 5));
+
+function getRestAfterDivision(x, y) {
+  x %= y;
+  return x;
+}
+//console.log(getRestAfterDivision(10, 5));
+console.log(increaseBalance(3000, 700));
+console.log(decreaseBalance(3000, 700));
+console.log(divideBalanceByAccounts(3000, 2, 4000));
+console.log(getRestAfterDivision(7000, 3));
