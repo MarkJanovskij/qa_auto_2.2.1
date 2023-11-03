03f848f4e301fae134c891fc3f0d96cb40cf956c
Mark <markyanovskiy@gmail.com>
2023-11-03 16:25:15 +0100

    update add increment and decrement dunction

--- increment and decrement.js
+++ increment and decrement.js
@@ -1,8 +1,25 @@
-function increment(x,y) {
-    x ++, y;
+
+
+function incrementAndDecrement(x, y) {
+    x++;
+    y--;
+    console.log("x after increment: " + x); // Output: x after increment: 
+    console.log("y after decrement: " + y); // Output: y after decrement: 
+  }
+
+  incrementAndDecrement(5, 5); // Output: x after increment: 6, y after decrement: 
+
+  function increment(x) {
+    x ++;
     return x;
+    }
+    
+    console.log(increment(5))
 
-
-}
-
-console.log(increment)
+    function decrement(x) {
+        x --;
+        return x;
+        }
+        
+        console.log(decrement(10))
+    // update add increment and decrement dunction
